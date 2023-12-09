c5676a48fb79b7a9c780df9a135eb4f70fdbe9eb
neoramic <neoramic@fel.cvut.cz>
2023-12-09 10:18:42 +0100

    když si člověk zkopíruje místo příkladu řešení, tak se pak zasekne :D

---
+++ 2023_09.py
@@ -0,0 +1,28 @@
+from aocd.models import Puzzle
+import numpy as np
+
+def recur(s):
+    if np.sum(np.abs(s)) == 0:
+        return 0
+    diff = s[1:] - s[:-1]
+    return recur(diff) + diff[-1]
+def solver_a(data, reverse=False):
+    data_split = data.split('\n')
+
+    sum = 0
+    for d in data_split:
+        sequence = np.array([int(n) for n in d.split(' ')], dtype=int)
+        if reverse:
+            sequence = sequence[::-1]
+        s = recur(sequence) + sequence[-1]
+        sum += s
+    print(sum)
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=2023, day=9)
+
+    print(puzzle.examples[0].input_data)
+    # solver_a(puzzle.examples[0].input_data)
+    # solver_a(puzzle.input_data)
+    solver_a(puzzle.examples[0].input_data, reverse=True)
+    solver_a(puzzle.input_data, reverse=True)
